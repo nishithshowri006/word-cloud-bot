aa048351af74a40cbffcbf6613e7f45bf3f9f27e
Nishith Showri <nishithshowri006@gmail.com>
2021-04-12 14:41:52 +0530

    Finished main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 from tweepy.streaming import Stream, StreamListener
+from word_cloud import *
 load_dotenv()
 CONSUMER_KEY = os.getenv("API_KEY")
 CONSUMER_SECRET = os.getenv("API_SECRET_KEY")
@@ -12,9 +13,25 @@ ACCESS_SECRET = os.getenv("ACCESS_SECRET")
 
 class MyListner(StreamListener):
 
-    def on_status(self, status):
-        print(status.__dir__())
-        return "nishith" 
+    def __init__(self, api:tweepy.API):
+        super().__init__(api=api) 
+        self.api = api
+        self.me = api.me()
+        self.key_words = ["Word-cloud","word cloud","Word Cloud"]
+
+
+    def on_status(self, status:tweepy.Status):
+        tweet_text = status.text
+        flag = any([True if key in tweet_text else False for key in self.key_words])
+        screen_name = status.user.screen_name
+        tweet_id = status.id_str
+        if flag:
+            username = status.user.screen_name
+            tweet_list = get_tweet_list(api,username)
+            df = make_df(tweet_list)
+            img = start_cloud(df)
+            reply(self.api,tweet_id,img,screen_name)
+        
 
 def get_tweet_list(api:tweepy.API,username:str) -> list:
     """
@@ -28,7 +45,6 @@ def get_tweet_list(api:tweepy.API,username:str) -> list:
         user_tweets = api.user_timeline(screen_name=username,max_id=oldest,count=200,include_rts=False,exclude_replies=True)
         tweet_list.extend(user_tweets)
         oldest = tweet_list[-1].id - 1
-        print(f"{len(tweet_list)} tweets are loaded so far")
     result = [str(tweet.text.split("http")[0]) for tweet in tweet_list]
     print("Got the list of tweets")
     return result
@@ -42,35 +58,30 @@ def make_df(tweets_list:list) ->pd.DataFrame:
     df = pd.DataFrame(tweets_list,columns=["text"])
     return df
 
-def get_saved_image(df:pd.DataFrame):
-    """
-    Okay I am assuming we call the word cloud start here.
-    """
-    pass
-
-def reply(api:tweepy.API, image:bytes):
+def reply(api:tweepy.API, tweet_id:str, image:str, screen_name:str):
     """
     I am assuming here is where when the bot is mentioned 
     it replies with the image.
     """
-    pass
+    api.update_with_media(status=f"@{screen_name}",filename = image,in_reply_to_status_id_str=tweet_id)
+    print("Done")
 
-def main():
+
+def main(api:tweepy.API,main_user):
     """
     Authentication and calling the helper function
     """
-    # listner = MyListner()
-    auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
-    # stream = Stream(auth,listner)
-    api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
-    main_user = api.get_user(screen_name="corner1705").id
-    # ans = stream.filter(follow=[f"{main_user}"],track=["word cloud","word-cloud"],is_async=True)
-    
-    # print(ans)
-    tweet_list = get_tweet_list(api,"corner1705")
-    df = make_df(tweet_list)
-    # image = get_saved_image(df)
+    listner = MyListner(api)
+    stream = Stream(auth,listner)
+    ans = stream.filter(follow=[f"{main_user}"],is_async=True)
+
 
 if __name__=="__main__":
-    main()
+    auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
+    api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
+    main_user = api.get_user(screen_name="corner1705").id_str
+    username = "coner1705"
+    if username:
+        main(api,main_user)
+    print("Exited")
